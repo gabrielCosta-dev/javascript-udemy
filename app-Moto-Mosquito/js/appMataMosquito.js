5de92fcf3226ff421a3725df7a4866f4f8d46368
Gabriel Lima <lima20gabriel@gmail.com>
2023-08-31 13:10:13 -0300

    update

--- app-Moto-Mosquito/js/appMataMosquito.js
+++ app-Moto-Mosquito/js/appMataMosquito.js
@@ -14,20 +14,32 @@ window.addEventListener('resize', ()=>{
 })
 
 //Posição
-let posiçãoX = Math.floor(Math.random() * largura) - 90
-let posiçãoY = Math.floor(Math.random() * altura) - 90
-posiçãoX = posiçãoX < 0 ? 0 : posiçãoX
-posiçãoY = posiçãoY < 0 ? 0 : posiçãoY
+function posicaoRandomica() {
+    //Verificar existencia da imagem mosquito
+    //Remover imagem mosquito anterior caso exista
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
 
-//Criar Imagem do Mosquito
-let img_mosquito = document.createElement('img')
-img_mosquito.setAttribute('src', 'imagens/mosquito.png')
-img_mosquito.setAttribute('class', tamanhoAleatorio())
-img_mosquito.style.left = posiçãoX + 'px'
-img_mosquito.style.top = posiçãoY + 'px'
-img_mosquito.style.position = 'absolute'
-document.body.appendChild(img_mosquito)
+    //Aleatorizar Posição
+    let posiçãoX = Math.floor(Math.random() * largura) - 90
+    let posiçãoY = Math.floor(Math.random() * altura) - 90
+    posiçãoX = posiçãoX < 0 ? 0 : posiçãoX
+    posiçãoY = posiçãoY < 0 ? 0 : posiçãoY
 
+    //Criar Imagem do Mosquito
+    let img_mosquito = document.createElement('img')
+    img_mosquito.setAttribute('src', 'imagens/mosquito.png')
+    img_mosquito.id = 'mosquito'
+    img_mosquito.classList.add(tamanhoAleatorio(), ladoAleatorio())
+    img_mosquito.style.left = posiçãoX + 'px'
+    img_mosquito.style.top = posiçãoY + 'px'
+    img_mosquito.style.position = 'absolute'
+    
+    document.body.appendChild(img_mosquito)
+}
+
+//Tamanho Imagem Mosquito
 function tamanhoAleatorio() {
     let classe = Math.floor(Math.random()*3)
     
@@ -42,4 +54,20 @@ function tamanhoAleatorio() {
             return 'mosquito3'
     }
 }
-console.log(tamanhoAleatorio())
+
+//Alternando Lado Da Imagem Mosquito
+function ladoAleatorio() {
+    let lado = Math.floor(Math.random()*2)
+    switch (lado) {
+        case 0:
+            return 'ladoA'
+    
+        case 1:
+            return 'ladoB'
+    }
+}
+
+//Adicionar e Remover Imagem Em Intervalo
+setInterval(() => {
+    posicaoRandomica()
+}, 1000);
